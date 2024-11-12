54e1bd969705423ef7d61ba404d7bb9197247245
Michael5366 <qwentin536@gmail.com>
2024-11-13 02:17:06 +0300

    feat: unbracketTag (Wed, Nov 13, 2024 02:17:06 AM)

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -469,8 +469,11 @@ function extractNameFromTemplate(value) {
  *   unbracketTag('<span>') => 'span'
  *   unbracketTag('<a>') => 'a'
  */
-function unbracketTag(/* str */) {
-  throw new Error('Not implemented');
+function unbracketTag(str) {
+  return str
+    .split('')
+    .filter((el) => el !== '>' && el !== '<')
+    .join('');
 }
 
 /**
