522394b5b884697562315e5c86b526edd0f7beab
Michael5366 <qwentin536@gmail.com>
2024-11-13 02:03:30 +0300

    feat: invertCase (Wed, Nov 13, 2024 02:03:30 AM)

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -404,8 +404,22 @@ function reverseWords(str) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  const low = str.toLowerCase();
+  const up = str.toUpperCase();
+  let result = '';
+
+  for (let i = 0; i < str.length; i += 1) {
+    const char = str[i];
+
+    if (char === low[i]) {
+      result += char.toUpperCase();
+    } else if (char === up[i]) {
+      result += char.toLowerCase();
+    }
+  }
+
+  return result;
 }
 
 /**
